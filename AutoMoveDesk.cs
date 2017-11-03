2fe912a6dbcc38c1d5bf3ac0ff4b2dd48b593527
AB3000 <31480939+AB3000@users.noreply.github.com>
2017-11-03 19:36:33 -0400

    Working animations/camera
    This branch has animations working currently for my game. I uploaded the Level1.Unity file along with scripts (whole project was too big too upload).

---
+++ AutoMoveDesk.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//inherits from class we were  working on (PhysicsObject)
+public class AutoMoveDesk : PhysicsObject {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetVelocity = Vector2.left;//moves to the left
+	}
+}
