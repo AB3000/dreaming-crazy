d3f39a6f82fbee4b955dbf64be237bb2e344e009
AB3000 <31480939+AB3000@users.noreply.github.com>
2017-11-12 12:41:00 -0500

    Add files via upload
    Level Change on trigger working

---
+++ Assets/scripts/DoorLevelChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLevelChange : MonoBehaviour {
+
+	public bool isActivated;
+	public static int level;
+	// Use this for initialization
+	void Start () {
+		level = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && isActivated) {
+			Application.LoadLevel ("Level2");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.name == "Andromeda") {
+			isActivated = true;
+		}
+			
+
+		//if (destroyWhenActivated) {
+			//Destroy (gameObject); 
+		//}
+
+		}
+
+
+	}
+
